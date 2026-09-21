16b8a7f39cf51c8ffd44bc785e070def5be1d098
yindeish <adeshinaadam03@gmail.com>
2026-09-21 08:14:32 +0100

    refactor: update API type definitions for micro-courses, learning, and rewards

--- src/app/api/types.ts
+++ src/app/api/types.ts
@@ -1,35 +1,47 @@
 type User = {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    jobTitle: string;
-    profilePictureUrl: string;
-    totalPoints: number;
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  jobTitle: string;
+  profilePictureUrl: string;
+  totalPoints: number;
 }
 
 type MicroCourse = {
-    title: string;
-    description: string;
-    createdAt: string;
-    authorId: string;
-  idMicroCourse Int @id @default (autoincrement())
-  category      String ?
-    videoUrl      String ?
-        pointsReward  Int @default (0)
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  authorId: string;
+  category: string;
+  videoUrl: string;
+  pointsReward: number;
 }
 
-type Learning = {
-
+type MyLearning = {
+  id: string;
+  userId: string;
+  courseId: string;
+  startDate: string;
+  status: 'in-progress' | 'completed'
 }
 
 
 type Reward = {
-
+  id: string;
+  name: string;
+  description: string;
+  pointsCost: number;
+  availableQuantity: number;
+  imageUrl?: string;
 }
 
 
-type RewardHistory = {
-
+type MyRewards = {
+  id: string;
+  userId: string;
+  rewardId: string;
+  redeemedAt: string;
 }
