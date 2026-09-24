065ebb010caf8b1ac32fb80fe051c752d7ba3379
yindeish <adeshinaadam03@gmail.com>
2026-09-24 08:33:27 +0100

    feat: add request body parsing and validation stubs in signup route

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -12,7 +12,7 @@ import { NextRequest, NextResponse } from "next/server";
 // 2. Request Params (Not sensitive and not much)
 // 3. Url Params (Hashed)
 
-
+// ! QA (Question and Answer)
 // !What are we doing?
 // !Register a user
 // !What do we need?
@@ -20,13 +20,66 @@ import { NextRequest, NextResponse } from "next/server";
 // !Where do we get them?
 // !From the request body
 // !How do we use them?
-// !Validation
 // ! We use them to create a profile  or an account for the user
 
+
+// !Steps
+// Create the controller function
+// Collect info from the body
+// Validate the info of the user (validation like spellings, ) for the fields and their values
+// Validate if the user already exist
+// Create the user
+// Return the user for the frontend
+
+
+// Destructuring in Javascript
+
+const person = {
+    name: 'Rehab',
+    age: 15,
+    married: true
+};
+
+// const name = person.name;
+// const age = person.age;
+// const married = person.married;
+
+
+// To avoid repetition
+// To maintain simplicity (To keep things simple)
+// 
+
+
+const { age, married } = person;
+
 export async function POST(request: NextRequest) {
+    // const { email, password, } = await request.json();
     const body = await request.json();
+    const { email, firstName, lastName, password } = body;
 
-    console.log({ body })
+    // email
+    // Email
+    // eMail
+    // emAil
+    // emaIl
+    // emaiL
+    // if (!email) {
+    //     return NextResponse.json({ msg: 'No email!' })
+    // }
+    // if (!firstName) {
+    //     return NextResponse.json({ msg: 'No first name!' })
+    // }
+    // if (!lastName) {
+    //     return NextResponse.json({ msg: 'No last name!' })
+    // }
+    // if (!password) {
+    //     return NextResponse.json({ msg: 'No password!' })
+    // }
 
-    return NextResponse.json('hey!')
+    type A = string | number;
+
+    if (!email || !firstName || !lastName || !password)
+
+        console.log({ email })
+    return NextResponse.json({ msg: email })
 }
